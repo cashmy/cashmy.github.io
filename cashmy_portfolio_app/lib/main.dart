56d1e5794e52cfd9c8326126e18bfee8cea26bc3
cashmy <cmyers880@gmail.com>
2021-03-10 12:19:17 -0600

    Add another font asset and fix demographic layout design issues

--- cashmy_portfolio_app/lib/main.dart
+++ cashmy_portfolio_app/lib/main.dart
@@ -45,12 +45,21 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title:
+        Text(widget.title),
       ),
+      backgroundColor: Colors.orange ,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            // Container to force column to full width of display
+            Container(
+              height: 30.0,
+              width: double.infinity,
+            ),
             CircleAvatar(
               radius: 50.0,
               backgroundImage: AssetImage('images/no-img.png'),
@@ -71,6 +80,24 @@ class _MyHomePageState extends State<MyHomePage> {
                 fontSize: 20.0,
                 color: Colors.black,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'cmyers880@gmail.com',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 12,
+                color: Colors.black,
+                letterSpacing: 3.2,
+                fontWeight: FontWeight.w700,
+                height: 1.5,
               ),
             ),
           ],
